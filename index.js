1ec482a44632504a98cc967017f5fdaf4af5c10d
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-08-24 12:25:41 -0700

    new added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Exercise 1
-let num1= 8;
+let num1 = 8;
 let num2 = 5;
 
 //addition
@@ -12,7 +12,7 @@ let substraction = num1 - num2;
 let multiplication = num1 * num2;
 
 //division
-let division = num1/num2;
+let division = num1 / num2;
 
 //result in console
 
@@ -20,21 +20,21 @@ console.log(addition);
 console.log(substraction);
 console.log(multiplication);
 console.log(Math.floor(division));
-console.log(Math.ceil(division))
+console.log(Math.ceil(division));
 
 //Exercise 2
 
-let greetings ="Good morning";
-let name ="Jean Claude";
+let greetings = "Good morning";
+let name = "Jean Claude";
 
-let greetingMessage = greetings + "," + " " + name +"!";
+let greetingMessage = greetings + "," + " " + name + "!";
 
 //document.write(greetingMessage);
 
 //Exercise 3
 
-document.getElementById("color-btn").addEventListener("click", () =>{
-    alert("My favorite color is black")
+document.getElementById("color-btn").addEventListener("click", () => {
+  alert("My favorite color is black");
 });
 
 //Exercise 4
@@ -45,26 +45,26 @@ let showTriple = myNumber * 3;
 let showHalf = myNumber / 2;
 
 //doubled-number
-document.getElementById("show-double").addEventListener("click", () =>{
-    console.log(showDouble);
+document.getElementById("show-double").addEventListener("click", () => {
+  console.log(showDouble);
 });
 
 //tripled-number
 
-document.getElementById("show-triple").addEventListener("click", () =>{
-    console.log(showTriple);
+document.getElementById("show-triple").addEventListener("click", () => {
+  console.log(showTriple);
 });
 
 //half-number
 
-document.getElementById("show-half").addEventListener("click", () =>{
-    console.log(Math.ceil(showHalf));
+document.getElementById("show-half").addEventListener("click", () => {
+  console.log(Math.ceil(showHalf));
 });
 
 //Exercise 5
 let count = 0;
 
-document.getElementById("count-clicks").addEventListener("click", () =>{
-    count+= 1;
-    console.log("clicked" + " " + count);
+document.getElementById("count-clicks").addEventListener("click", () => {
+  count += 1;
+  console.log("clicked" + " " + count);
 });
